ec5b6a1fd1d2527e18791a81f6e044da63fbe749
milescattini <milescattini@gmail.com>
2020-04-02 23:15:14 +0100

    Simplified name
    Adding the name of a VM to a sub-property adds no value

--- Generic-VM/extension_domain_join.tf
+++ Generic-VM/extension_domain_join.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_machine_extension" "vm_domain_join" {
   count                = var.enable_domain_join ? 1 : 0
   depends_on = [azurerm_virtual_machine.vm_virtualmachine]
-  name                 = "${var.vm_name}_domain_join"
+  name                 = "DomainJoin"
   virtual_machine_id   = azurerm_virtual_machine.vm_virtualmachine.id
   publisher            = "Microsoft.Compute"
   type                 = "JsonADDomainExtension"
